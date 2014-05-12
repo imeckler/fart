6a4c5e670e047441c2c5d9d5e862d47508de10ff
Izaak Meckler <ihmeckler@gmail.com>
2014-05-12 14:14:07 -0500

    working

--- ocaml/src/arrows.ml
+++ ocaml/src/arrows.ml
@@ -6,15 +6,10 @@ let arrow radius = let open Frp.Behavior in let open Draw in
   circle (return radius) (return (20., 20.))
     ~props:[|return (Property.fill (Color.random ()))|]
 
-let mom_text font_size = let open Draw in let open Frp.Behavior in
-  let msg = [|"Happy"; "Mother's"; "Day!"|].(Random.int 3) in
-  text (return msg) (return (20., 20.)) 
-    ~props:[|return (Property.any ~name:"font-size" ~value:(string_of_int font_size))|]
-(*
-  Draw.image ~width:(return 200) ~height:(return 60)
-    (return "art/arrow.svg") *)
+(* TODO: Find out why we need a new one each time *)
+let arrow () = Draw.svg_file "art/arrow.svg"
 
-let width, height = 1000, 1000
+let width, height = 1000, 500
 let width', height' = float_of_int width, float_of_int height
 
 let flying_arrow () = let open Draw in
@@ -25,14 +20,12 @@ let flying_arrow () = let open Draw in
     in Color.of_rgb ~r:c ~g:c ~b:c () in *)
   let radius = (7. *. (c /. 1.5)) ** 2. in
   let speed = c *. 0.5 in
-  let font_size = int_of_float (c *. 20.) in
   let y_pos = float_of_int (Random.int height) in
   let trans_x = Frp.(scan (Stream.deltas 30.) ~init:0. ~f:(fun x t ->
     x +. Time.Span.to_ms t *. speed
   ))
   in
-  let composite = pictures [|arrow radius; mom_text font_size;|] in
-  ( transform composite (Frp.Behavior.map trans_x ~f:(fun x ->
+  ( transform (arrow ()) (Frp.Behavior.map trans_x ~f:(fun x ->
     [| Transform.Translate (x, y_pos) |]))
   , trans_x
   )
